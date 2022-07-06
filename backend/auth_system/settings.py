00a61777eb086111dbfd5c5785f005f1f7c16969
Prabhat Kumar Singh Gaur <64524919+pksg2001@users.noreply.github.com>
2022-07-06 13:19:31 +0530

    Update settings.py

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pksg2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'bidqvefhoiqurrla'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
