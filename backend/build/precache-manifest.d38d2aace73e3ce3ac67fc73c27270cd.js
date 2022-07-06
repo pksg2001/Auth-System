a297c87e22873c2eba2ababe40a62fff3567a6bb
Prabhat <pksg2001@gmail.com>
2022-07-06 11:34:07 +0530

    Initial commit

---
+++ backend/build/precache-manifest.d38d2aace73e3ce3ac67fc73c27270cd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb9531ac4d554357eb31",
+    "url": "/static/js/main.fb9531ac.chunk.js"
+  },
+  {
+    "revision": "b5567650f5bcf4634977",
+    "url": "/static/js/1.b5567650.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c88c0230bae6e2b29de2a93d79b2ea2c",
+    "url": "/index.html"
+  }
+];
